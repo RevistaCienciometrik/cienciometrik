abb61840b3f3e48133e0ca0bc68144fa369fe6d7
mapachogithub <fraquiones11@misena.edu.co>
2025-04-22 11:44:38 -0500

    métricas:
    se añade un contador de visitas con un botón de reseteo, en html y js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// MÉTRICAS: 
 document.addEventListener('DOMContentLoaded', function() {
     const metricas = {
         total_articulos: 185,
@@ -9,3 +10,36 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('total-volumenes').textContent = metricas.total_volumenes;
     document.getElementById('total-semilleros').textContent = metricas.total_semilleros;
 });
+
+// ----------------------------------------------------------------------------------------------------
+
+    // CONTADOR DE VISITAS
+    
+document.addEventListener('DOMContentLoaded', function() {
+
+    let visitas = localStorage.getItem('visitasCienciometrik');
+
+    if (visitas) {
+        visitas = parseInt(visitas) + 1;
+    } else {
+        visitas = 1;
+    }
+
+    localStorage.setItem('visitasCienciometrik', visitas);
+    const visitasElement = document.getElementById('total-visitas');
+    if (visitasElement) {
+        visitasElement.textContent = visitas;
+    }
+
+    // Lógica para el botón de resetear
+    const resetButton = document.getElementById('reset-visitas');
+    if (resetButton) {
+        resetButton.addEventListener('click', function() {
+            localStorage.removeItem('visitasCienciometrik');
+            if (visitasElement) {
+                visitasElement.textContent = 0; // O 'Cargando...' si quieres simular una nueva carga
+            }
+        });
+    }
+});
+// ----------------------------------------------------------------------------------------
